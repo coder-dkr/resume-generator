0a045f6f54ebef98dde06a9c8bb45e04f90841e4
Dhruv Roy <dhruvakr9001@gmail.com>
2025-08-02 17:20:52 +0530

    clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { FormProvider } from '@/app/context/FormContext';
+import { FormProvider } from "@/app/context/FormContext";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-         <FormProvider>
-        {children}
-        </FormProvider>
+        <FormProvider>{children}</FormProvider>
       </body>
     </html>
   );
